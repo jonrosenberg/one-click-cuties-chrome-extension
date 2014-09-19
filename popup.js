9948e6061971072d28320c5f913f17c60d056af1
Jonathan Rosenberg <jon.rosenberg@gmail.com>
2014-09-19 15:13:33 -0500

    one click Puppies init code submit

---
+++ popup.js
@@ -0,0 +1,83 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * Global variable containing the query we'd like to pass to Flickr. In this
+ * case, puppies!
+ *
+ * @type {string}
+ */
+var QUERY = 'puppies';
+
+var kittenGenerator = {
+  /**
+   * Flickr URL that will give us lots and lots of whatever we're looking for.
+   *
+   * See http://www.flickr.com/services/api/flickr.photos.search.html for
+   * details about the construction of this URL.
+   *
+   * @type {string}
+   * @private
+   */
+  searchOnFlickr_: 'https://secure.flickr.com/services/rest/?' +
+      'method=flickr.photos.search&' +
+      'api_key=3f22a05791b2146277e51867d854ecb8&' +
+      'text=' + encodeURIComponent(QUERY) + '&' +
+      'safe_search=1&' +
+      'content_type=1&' +
+      'sort=interestingness-desc&' +
+      'per_page=20',
+
+  /**
+   * Sends an XHR GET request to grab photos of lots and lots of puppies. The
+   * XHR's 'onload' event is hooks up to the 'showPhotos_' method.
+   *
+   * @public
+   */
+  requestPuppies: function() {
+    var req = new XMLHttpRequest();
+    req.open("GET", this.searchOnFlickr_, true);
+    req.onload = this.showPhotos_.bind(this);
+    req.send(null);
+  },
+
+  /**
+   * Handle the 'onload' event of our kitten XHR request, generated in
+   * 'requestPuppies', by generating 'img' elements, and stuffing them into
+   * the document for display.
+   *
+   * @param {ProgressEvent} e The XHR ProgressEvent.
+   * @private
+   */
+  showPhotos_: function (e) {
+    var puppies = e.target.responseXML.querySelectorAll('photo');
+    for (var i = 0; i < puppies.length; i++) {
+      var img = document.createElement('img');
+      img.src = this.constructPuppiesURL_(puppies[i]);
+      img.setAttribute('alt', puppies[i].getAttribute('title'));
+      document.body.appendChild(img);
+    }
+  },
+
+  /**
+   * Given a photo, construct a URL using the method outlined at
+   * http://www.flickr.com/services/api/misc.urlKittenl
+   *
+   * @param {DOMElement} A kitten.
+   * @return {string} The kitten's URL.
+   * @private
+   */
+  constructPuppiesURL_: function (photo) {
+    return "http://farm" + photo.getAttribute("farm") +
+        ".static.flickr.com/" + photo.getAttribute("server") +
+        "/" + photo.getAttribute("id") +
+        "_" + photo.getAttribute("secret") +
+        "_s.jpg";
+  }
+};
+
+// Run our kitten generation script as soon as the document's DOM is ready.
+document.addEventListener('DOMContentLoaded', function () {
+  kittenGenerator.requestPuppies();
+});
