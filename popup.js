bae99723a240c0b49c436ae9d582ff2ee834361a
Jonathan Rosenberg <jon.rosenberg@gmail.com>
2014-11-12 23:44:25 -0600

    replaced flicker with cuteness emergancy

--- popup.js
+++ popup.js
@@ -14,7 +14,8 @@ var pages = 999;
 var sort = 'interestingness-desc';
 var isBrowsing = true;
 var target;
-var puppieGenerator = {
+var hURL = "https://oneclickcuties.herokuapp.com/api/tweetsmedia/CuteEmergency?count=20";
+var occ = {
   /**
    * Flickr URL that will give us lots and lots of whatever we're looking for.
    *
@@ -38,51 +39,62 @@ var puppieGenerator = {
     },
 
   /**
-   * Sends an XHR GET request to grab photos of lots and lots of puppies. The
+   * Sends an XHR GET request to grab photos of lots and lots of cuties. The
    * XHR's 'onload' event is hooks up to the 'showPhotos_' method.
    *
    * @public
    */
-  requestPuppies: function() {
-    var req = new XMLHttpRequest();
-    $( "#results" ).undelegate( "img", "click", puppieGenerator.showPhoto_ );
-    req.open("GET", this.searchOnFlickr_(), true);
-    req.onload = this.showPhotos_.bind(this);
-    req.send(null);
-    $( "#results" ).delegate( "img", "click", puppieGenerator.showPhoto_ );
+  requestCuties: function() {
+    console.log(this.searchOnFlickr_());
+
+
+    var req = $.getJSON( hURL );
+    req.done(function(data) {
+      console.log( "second success" );
+      console.log( data);
+      occ.showPhotos_(data);
+    });
+    req.fail(function(err) {
+      console.log( "error" );
+      console.log( err );
+    });
+    req.always(function() {
+      console.log( "complete" );
+    });
+
   },
 
   /**
-   * Handle the 'onload' event of our puppie XHR request, generated in
-   * 'requestPuppies', by generating 'img' elements, and stuffing them into
+   * Handle the 'onload' event of our cutie XHR request, generated in
+   * 'requestCuties', by generating 'img' elements, and stuffing them into
    * the document for display.
    *
    * @param {ProgressEvent} e The XHR ProgressEvent.
    * @private
    */
-  showPhotos_: function (e) {
-    target = e.target;
+  showPhotos_: function (data) {
+    
     $('#results img').remove();
-    pages = $( e.target.responseXML ).find('photos').attr('pages');
-    var puppies = e.target.responseXML.querySelectorAll('photo');
-    for (var i = 0; i < puppies.length; i++) {
+    //pages = $( e.target.responseXML ).find('photos').attr('pages');
+    var cuties = data.media;
+    for (var i = 0; i < cuties.length; i++) {
       var img = document.createElement('img');
-      img.src = this.constructPuppiesURL_(puppies[i]);
-      img.setAttribute('alt', puppies[i].getAttribute('title'));
-      img.setAttribute('data-m-url', puppies[i].getAttribute('url_m'));
+      img.src = cuties[i].media_url;
+      //img.setAttribute('alt', cuties[i].getAttribute('title'));
+      img.setAttribute('data-m-url', cuties[i].media_url);
       $('#results').append(img);
     }
   },
 
   /**
    * Given a photo, construct a URL using the method outlined at
-   * http://www.flickr.com/services/api/misc.urlPuppiel
+   * http://www.flickr.com/services/api/misc.urlCuties
    *
-   * @param {DOMElement} A puppie.
-   * @return {string} The puppie's URL.
+   * @param {DOMElement} A cutie.
+   * @return {string} The cuties URL.
    * @private
    */
-  constructPuppiesURL_: function (photo) {
+  constructCutiesURL_: function (photo) {
     return "http://farm" + photo.getAttribute("farm") +
         ".static.flickr.com/" + photo.getAttribute("server") +
         "/" + photo.getAttribute("id") +
@@ -122,10 +134,10 @@ var puppieGenerator = {
   }
 };
 
-// Run our puppie generation script as soon as the document's DOM is ready.
+// Run our cutie generation script as soon as the document's DOM is ready.
 document.addEventListener('DOMContentLoaded', function () {
-  puppieGenerator.requestPuppies();
-
+  occ.requestCuties();
+  
   // Click search or page
   $('footer span').bind( "click", function() {
     if($( this ).hasClass('search')){
@@ -134,8 +146,8 @@ document.addEventListener('DOMContentLoaded', function () {
     } else {
       page = $( this ).data("page");
     }
-    puppieGenerator.updatePage_();
-    puppieGenerator.requestPuppies();
+    occ.updatePage_();
+    occ.requestCuties();
     $( "#results" ).delegate( "img", "click", expandPhoto_ );
 
   });
@@ -144,8 +156,8 @@ document.addEventListener('DOMContentLoaded', function () {
   $('footer select').on( "change", function() {
     page = 1;
     sort = $( this ).val();
-    puppieGenerator.updatePage_();
-    puppieGenerator.requestPuppies();
+    occ.updatePage_();
+    occ.requestCuties();
   } )
 
   $( "body" ).keydown(function( event ) {
@@ -158,23 +170,23 @@ document.addEventListener('DOMContentLoaded', function () {
         QUERY = 'puppies';
       }
       page = 1;
-      puppieGenerator.updatePage_();
-      puppieGenerator.requestPuppies();
+      occ.updatePage_();
+      occ.requestCuties();
     } 
     // left arrow
     else if (isBrowsing &event.which == 37 && page != 1) {
       page -= 1;
-      puppieGenerator.updatePage_();
-      puppieGenerator.requestPuppies();
+      occ.updatePage_();
+      occ.requestCuties();
     }
     // right arrow
     else if (isBrowsing & event.which == 39 && page != pages) {
       page += 1;
-      puppieGenerator.updatePage_();
-      puppieGenerator.requestPuppies();
+      occ.updatePage_();
+      occ.requestCuties();
     }
 
   });
-  $("#photo").bind("click", puppieGenerator.hidePhoto_ );
+  $("#photo").bind("click", occ.hidePhoto_ );
 
 });
